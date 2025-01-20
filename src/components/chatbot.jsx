8b46bd466efc0930546a4640e361a4ce8d828f25
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2025-01-20 16:14:02 -0500

    feat: :sparkles: The chat box is already styled and the input is cleared after the message is sent

--- src/components/chatbot.jsx
+++ src/components/chatbot.jsx
@@ -16,37 +16,46 @@ const ChatBot = () => {
           message: userMessage,
         }),
       });
-
+      console.log(userMessage)
+      setUserMessage('')
+      console.log(userMessage)
       // Procesar la respuesta del bot
-    const data = await response.json();
+      const data = await response.json();
 
-    // Verificar si hay múltiples respuestas
-    let botMessage = '';
-    data.forEach(item => {
-      if (item.text) {
-        botMessage += item.text + ' ';
-      }
-    });
+      // Verificar si hay múltiples respuestas
+      let botMessage = '';
+      data.forEach(item => {
+        if (item.text) {
+          botMessage += item.text + ' ';
+        }
+      });
 
-    setBotResponse(botMessage.trim());
+      setBotResponse(botMessage.trim());
     } catch (error) {
       console.error('Error al enviar el mensaje:', error);
     }
   };
 
+
   return (
-    <div>
-      <h1>ChatBot</h1>
-      <div>
+    <div className='bg-gray-100 w-[500px] h-[500px] border flex flex-col'>
+      <div className='h-[20%] w-[100%] bg-white flex items-center px-10'>
+        <img src="" alt="" className='h-[50px] w-[50px] bg-black rounded-full'/>
+        <h3 className='text-xl font-medium ml-6'>Solvo Ai Chat</h3>
+      </div>
+      <div className='h-[68%]'>
+        {botResponse && <div><strong>Bot:</strong> {botResponse}</div>}
+      </div>
+      <div className='h-[12%] w-[100%] bg-white flex justify-between items-center px-5'>
         <input
           type="text"
           value={userMessage}
           onChange={(e) => setUserMessage(e.target.value)}
           placeholder="Escribe un mensaje"
+          className='text-lg'
         />
-        <button onClick={sendMessage}>Enviar</button>
+        <button onClick={sendMessage} className='bg-transparent'><i className='bx bxs-send bx-md'></i></button>
       </div>
-      {botResponse && <div><strong>Bot:</strong> {botResponse}</div>}
     </div>
   );
 };
