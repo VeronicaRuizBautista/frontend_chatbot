826b8cc7e006567dbc6b92772525671f3948c582
VeronicaRuizBautista <156429590+VeronicaRuizBautista@users.noreply.github.com>
2025-01-21 10:38:46 -0500

    feat: :sparkles: Button visibility functionality

--- src/components/pag.jsx
+++ src/components/pag.jsx
@@ -3,8 +3,10 @@ import Chatbot from "../components/chatbot.jsx";
 
 const Pag = () => {
     const [isVisible, setIsVisible] = useState(false);
+    const [isVisibleBtn, setIsVisibleBtn] = useState(true);
     const toggleChatbot = () => {
         setIsVisible(!isVisible);
+        setIsVisibleBtn(!isVisibleBtn)
     };
 
     return (
@@ -15,13 +17,15 @@ const Pag = () => {
                 <img className="img object-contain w-full h-full" src='../../public/img/robot.jpg' alt="robot"/>
             </div>
             </div>
-            <button 
-            onClick={toggleChatbot}
-            className=" btn bg-medium-orange w-16 h-16 rounded-full fixed bottom-4 right-4 flex items-center justify-center cursor-pointer border-medium-orang hover:border-dark-orange hover:bg-dark-orange focus:outline-none">
-                <i className='bx bxs-bot fas fa-heart text-4xl' style={{color:'#f3f0f0'}}></i>
-            </button>
+            {isVisibleBtn && (
+                <button 
+                onClick={toggleChatbot}
+                className=" btn bg-medium-orange w-16 h-16 rounded-full fixed bottom-4 right-4 flex items-center justify-center cursor-pointer border-medium-orang hover:border-dark-orange hover:bg-dark-orange focus:outline-none">
+                    <i className='bx bxs-bot fas fa-heart text-4xl' style={{color:'#f3f0f0'}}></i>
+                </button>
+            )}
             {isVisible && (
-                <div className="fixed bottom-20 right-5 w-80 bg-white shadow-lg rounded-lg p-4 z-10">
+                <div className="fixed bottom-10 right-5 max-w-[500px] bg-white shadow-lg rounded-lg p-4 z-10">
                     <Chatbot onClose={toggleChatbot} />
                 </div>
             )}
