586c382b65ca8744e883bbed693d4edb1238e093
VeronicaRuizBautista <156429590+VeronicaRuizBautista@users.noreply.github.com>
2025-01-21 09:47:08 -0500

    feat: :sparkles: desing of the main page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Chatbot from "./components/chatbot.jsx"
+import Pag from "./components/pag.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="App">
-        <h1>Solvo chatbot</h1>
-        <Chatbot />
-      </div>
+      <Pag></Pag>
+
     </>
   )
 }
